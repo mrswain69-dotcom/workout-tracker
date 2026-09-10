583c8d39077df29c0e4a4273a72c437bbaef1bff
mrswain69-dotcom <mrswain69@gmail.com>
2026-09-10 16:22:41 +0100

    Pin Stage 3 leaderboard Edge deployment source

---
+++ supabase/functions/group-xp-leaderboard/deploy.ts
@@ -0,0 +1,2 @@
+// Stage 3 deploy entrypoint. Pin the complete scorer bundle to the audited immutable Git commit.
+import "https://raw.githubusercontent.com/mrswain69-dotcom/workout-tracker/95eb9492a132b73f6a926dbe44d8da1175d3e164/supabase/functions/group-xp-leaderboard/index.ts";
