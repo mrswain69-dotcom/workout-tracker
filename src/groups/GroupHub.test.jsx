856266985b2791df851ee999c79bc545b055e80d
mrswain69-dotcom <mrswain69@gmail.com>
2026-09-11 15:21:07 +0100

    Isolate GroupHub tests from Stage 7 team child

--- src/groups/GroupHub.test.jsx
+++ src/groups/GroupHub.test.jsx
@@ -27,6 +27,10 @@ vi.mock("./GroupSeasons.jsx", () => ({
   default: () => null,
 }));
 
+vi.mock("./GroupTeamView.jsx", () => ({
+  default: () => null,
+}));
+
 import GroupHub from "./GroupHub.jsx";
 import * as groupDb from "./groupDb";
 
